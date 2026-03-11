397ada47e903c9fbdd8f39bc7c435c63e6557c04
SelebSolomon <selebsolomon@gmail.com>
2026-03-11 17:04:15 +0100

    minor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DomainModule } from './modules/domain.module'
+import { DomainModule } from './modules/domain.module';
 // import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [
-    DomainModule,
-
-  ],
+  imports: [DomainModule],
   controllers: [AppController],
   providers: [AppService],
 })
+//
 export class AppModule {}
